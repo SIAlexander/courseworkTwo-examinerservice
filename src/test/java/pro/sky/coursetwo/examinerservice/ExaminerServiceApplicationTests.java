b76a64ee8e053ef5fd061b3dc215e269c3f69a4a
Smokvin <alex.smokvin@gmail.com>
2023-07-13 16:14:44 +0300

    initial commit

---
+++ src/test/java/pro/sky/coursetwo/examinerservice/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursetwo.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
